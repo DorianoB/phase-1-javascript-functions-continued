ff773dc0142532da8b9db210577f470ac727144a
Doriano <doriano.bergamin18@gmail.com>
2024-01-17 22:00:59 +1100

    functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(monday = "go to the office"){
+    return `This Monday, I will ${monday}.`;
+}
+
+function wrapAdjective(name){
+return function(random){
+    return "You are " +name + random+ name+"!"
+}
+}
+
+// function saturdayFun(activity = "roller-skate"){
+//     console.log(`This Saturday, I want to ${activity}!`)
+// }
+
+// function mondayWork(monday){
+//     if (monday === 'go to the office'){
+//     return `This Monday, I will ${monday}!`;
+//     } else if (typeof monday === 'string')
+//     return 'This Monday, I will work from home!'
+// }
